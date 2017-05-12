a0f41550b6811ca7ea5d35156f7d54338b239e56
Filip Veljanovski <filipveljanovski@Filips-MacBook-Pro.local>
2017-05-12 21:16:39 +1000

    start

---
+++ Project 3 copy/assets/js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+        $('.author').mouseenter(function() {
+          $(this).addClass('enlarge');
+        });
+
+        $(".piracy").on('click', function(){
+             window.location = "http://rhizome.org/editorial/2012/apr/19/impermanent-book/?key=44098";
+        });
+
+        $(".1").on('click', function(){
+             window.location = "Books.html";
+        });
+
+        $(".2").on('click', function(){
+             window.location = "Paper.html";
+        });
+
+        $(".3").on('click', function(){
+             window.location = "Parchment.html";
+        });
+
+        $(".4").on('click', function(){
+             window.location = "Hamlet.html";
+        });
+
+        $(".5").on('click', function(){
+             window.location = "William.html";
+        });
+
+        $(".6").on('click', function(){
+             window.location = "China.html";
+        });
+
+        $(".7").on('click', function(){
+             window.location = "TangDynasty.html";
+        });
+
+        $(".8").on('click', function(){
+             window.location = "tea.html";
+        });
+
+        $(".9").on('click', function(){
+             window.location = "Papermaking.html";
+        });
+
+        $(".10").on('click', function(){
+             window.location = "Pulp.html";
+        });
+
+        $(".11").on('click', function(){
+             window.location = "index.html";
+        });
+
+        $(".12").on('click', function(){
+             window.location = "written.html";
+        });
+
+        $(".13").on('click', function(){
+             window.location = "comedy.html";
+        });
+
+        $(".14").on('click', function(){
+             window.location = "script.html";
+        });
+
+        $('.history').on('click', function() {
+          $('.thumbnail').addClass('showme');
+        });
+
+        $('.history').on('click', function() {
+          $('.thumbnail').removeClass('hideme');
+        });
+
+
+  });
